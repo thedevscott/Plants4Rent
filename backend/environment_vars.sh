049ea3e929710416391edc45dd8a1b1bb5fe5ff0
Omar Scott <thedevscott@gmail.com>
2020-01-05 22:15:03 -0500

    cleanup ENV variables

--- backend/environment_vars.sh
+++ backend/environment_vars.sh
@@ -1,7 +1,7 @@
 # Development server
-FLASK_APP = backend/app.py
-FLASK_ENV = development
-FLASK_DEBUG = 1
+FLASK_APP=backend/app.py
+FLASK_ENV=development
+FLASK_DEBUG=1
 
 # AUTH0 Setup
 AUTH0_DOMAIN="thedevscott.auth0.com"
@@ -14,8 +14,7 @@ DATABASE_HOST="localhost:5432"
 DATABASE_PATH="postgres://localhost:5432/plant_catalog"
 
 # For test_app.py
-RENTER_TOKEN="<VALID_JWT"
+RENTER_TOKEN="<VALID_JWT>"
 OWNER_TOKEN="<VALID_JWT>"
-DATABASE_TEST_HOST="localhost:5432"
 DATABASE_TEST_NAME="plant_catalog_test"
 DATABASE_TEST_PATH="postgresql://localhost:5432/plant_catalog_test"
